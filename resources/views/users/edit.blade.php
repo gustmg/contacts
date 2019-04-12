6b870156767f1a5c955491985d8ce38ac125e104
gustmg <gustavo.mitre.gallardo@gmail.com>
2019-04-11 22:28:47 -0500

    Profile picture button added

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,15 +5,8 @@
     <div class="row">
         <div class="col-md-6 text-center">
             <img src="{{ asset('img/profile_picture.png') }}" width="200"><br><br>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-                </div>
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-                    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-                </div>
-            </div>
+            <input type="file" name="file" id="file" class="upload" />
+            <label for="file">Choose a file</label>
         </div>
         <div class="col-md-4">
             <form>
